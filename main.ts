c985b161c43a5cc063fb16c84a791269f8c1c9e1
NamPE286 <nambuihung654@gmail.com>
2023-07-26 05:37:52 +0700

    update

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Hono } from 'https://deno.land/x/hono@v3.3.1/mod.ts'
 import { levelRoute } from './routes/level.ts'
 import { playerRoute } from './routes/player.ts'
 import { listRoute } from './routes/list.ts'
-import { apiSpec } from './utils/apiSpec.ts'
+// import { apiSpec } from './utils/apiSpec.ts'
 
 const app = new Hono()
 
@@ -10,9 +10,9 @@ app.get('/', (ctx) => {
     return ctx.json({ timestamp: new Date() })
 })
 
-app.get('/docs.json', (ctx) => {
-    return ctx.json(apiSpec)
-})
+// app.get('/docs.json', (ctx) => {
+//     return ctx.json(apiSpec)
+// })
 
 app.route('/level', levelRoute)
 app.route('/player', playerRoute)
