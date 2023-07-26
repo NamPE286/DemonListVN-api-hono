93ed3a28a1c323e469e49bde8a6bfe74aa3c269d
NamPE286 <nambuihung654@gmail.com>
2023-07-26 21:34:06 +0700

    revert

--- env.ts
+++ env.ts
@@ -4,11 +4,7 @@ import { load } from "https://deno.land/std@0.195.0/dotenv/mod.ts";
 async function getEnv(key: string) {
     try{
         console.log(`Using ${key} in .env file`)
-        const res = (await load())[key]
-        if(!res) {
-            console.warn(`Failed to load ${key} from .env file. Using ${key} in system env`)
-            return Deno.env.get(key)
-        }
+        return (await load())[key] || Deno.env.get(key)
     }
     catch {
         console.warn(`Failed to load ${key} from .env file. Using ${key} in system env`)
