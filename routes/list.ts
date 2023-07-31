7a3b8e5842e277effa2ce91edfe92b12953dcd14
NamPE286 <nambuihung654@gmail.com>
2023-07-31 23:15:50 +0700

    GET /list/:type/progress/:uid done

--- routes/list.ts
+++ routes/list.ts
@@ -54,7 +54,64 @@ listRoute.get('/:type', async (ctx) => {
     try {
         return ctx.json(await list.fetchLevels(sortBy, start, end))
     }
-    catch(err) {
+    catch (err) {
+        console.error(err)
+        ctx.status(500)
+    }
+})
+
+/**
+ * @openapi
+ * /list/{type}/progress/{uid}:
+ *  get:
+ *      tags:
+ *      - Level
+ *      summary: Get levels by list
+ *      parameters:
+ *        - name: type
+ *          in: path
+ *          description: The type of list
+ *          required: true
+ *          schema:
+ *              type: string
+ *        - name: uid
+ *          in: path
+ *          description: UUID of player
+ *          required: true
+ *          schema:
+ *              type: string
+ *        - name: start
+ *          in: query
+ *          description: Range start index
+ *          required: false
+ *          schema:
+ *              type: integer
+ *        - name: end
+ *          in: query
+ *          description: Range end index
+ *          required: false
+ *          schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Return an dictionary of progress
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *          500:
+ *              description: Failed operation
+ */
+listRoute.get('/:type/progress/:uid', async (ctx) => {
+    const type = ctx.req.param('type')
+    const start = parseInt(ctx.req.query('start')!) || 0
+    const end = parseInt(ctx.req.query('end')!) || 50
+    const uid = String(ctx.req.param('uid'))
+    const list = new List(type)
+
+    try {
+        return ctx.json(await list.fetchProgress(uid, start, end))
+    }
+    catch (err) {
         console.error(err)
         ctx.status(500)
     }
