b601396892332a9c13e0a0b8ce1516edb7f2bd66
NamPE286 <nambuihung654@gmail.com>
2023-08-23 20:44:10 +0700

    /search documentation

--- routes/search.ts
+++ routes/search.ts
@@ -3,6 +3,27 @@ import Search from '@classes/Search.ts'
 
 export const searchRoute = new Hono()
 
+/**
+ * @openapi
+ * /search/{query}:
+ *  get:
+ *      tags:
+ *      - Search
+ *      summary: Search levels and players
+ *      parameters:
+ *        - name: query
+ *          in: path
+ *          description: The search query
+ *          required: true
+ *          schema:
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: Return a levels and player list
+ *              content:
+ *                  application/json:
+ *                      schema:
+ */
 searchRoute.get('/:query', async (ctx) => {
     const query = ctx.req.param('query')
     const limit = parseInt(ctx.req.query('limit')!) || 10
